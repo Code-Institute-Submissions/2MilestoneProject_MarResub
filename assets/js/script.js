3af76ca2dd37baccaf460f9c779047e0a43191cd
SaraSanchezz <sarasanrodrigo@gmail.com>
2020-12-08 17:42:29 +0000

    Reorganised map locations

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,61 +50,68 @@ window.onload = function() {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
 };
-
-
-
-
-
 
 
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const colombia = { lat: 11.24079, lng: -74.19904 };
-  // The map, centered at colombia
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: colombia,
-  });
-  // The marker, positioned at colombia
-  const marker = new google.maps.Marker({
-    position: colombia,
-    map: map,
-  });
-}
-// Email validation
-function validateForm() {
-  var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
-  }
-  var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
-  } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
-  }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var message =  document.getElementById('message').value;
-  if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
-  }
-  document.querySelector('.status').innerHTML = "Sending...";
-}
+  var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 5,
+      center: {
+          lat: 46.619261,
+          lng: -33.134766
+       }
+     });
+
+  // The markers
+  var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+            var locations = [{
+                lat: -32.4425900,
+                lng: -63.2258900
+            }, {
+                lat: 45.8144400,
+                lng: 15.9779800
+            }, {
+                lat: 11.24079 ,
+                lng: -74.19904
+            }];
+
+            var markers = locations.map(function(location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length]
+                });
+            });
+
+            var markerCluster = new MarkerClusterer(map, markers, {
+                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+            });
+        }
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Send Email
+function sendMail(conctactForm){
+    emailjs.send("Gmail","template_8oourjn",{
+        "from_name": conctactForm.name.value,
+        "from_email": conctactForm.emailaddress.value,
+        "info_request": contactForm.infosummary.value,
+       
+        return: false
+    });
+} 
